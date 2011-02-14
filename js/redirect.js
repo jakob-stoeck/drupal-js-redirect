5963956e9c6a4ecbd409ca029c3ea7ad086b3c68
Jakob Stoeck <kontakt@stoeck.it>
2011-02-14 18:19:17 +0100

    added possibility to choose equality operator with js redirect

--- js/redirect.js
+++ js/redirect.js
@@ -1,9 +1,30 @@
 jQuery(document).ready(function() {
 	// redirect javascript clients to a richer page
 	if(Drupal.settings.js_redirect.mapping.length > 0) {
+		var equalOp = '';
 		$.each(Drupal.settings.js_redirect.mapping, function(index, link) {
-			console.log([link.noscript, link.enhanced]);
-			$('a[href=' + link.noscript + ']').click(function(e) {
+			var type = link['type'] || 'is equal';
+			switch(type) {
+				case 'is equal':
+					equalOp = '=';
+					break;
+				case 'not equal':
+					equalOp = '!=';
+					break;
+				case 'starts with':
+					equalOp = '^=';
+					break;
+				case 'ends with':
+					equalOp = '$=';
+					break;
+				case 'contains':
+					equalOp = '*=';
+					break;
+				default:
+					equalOp = '=';
+			};
+				
+			$('a[href' + equalOp + '"' + link.noscript + '"' + ']').click(function(e) {
 				window.location = link.enhanced;
 				e.preventDefault();
 			});
